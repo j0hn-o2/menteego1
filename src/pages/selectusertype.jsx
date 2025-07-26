e0cad03ee383506f287eaaf23f4a0bcbcaa6c9ea
John <jpaappiah@gmail.com>
2025-07-26 15:07:44 +0000

    10th commit

--- src/pages/selectusertype.jsx
+++ src/pages/selectusertype.jsx
@@ -22,22 +22,27 @@ function SelectUserType() {
   return (
     <div className="select-user-type">
 
-      <div className="select-user-type-logo">
-        <img src={menteegologo} alt="MenteeGo Logo" />
-      </div>
-      
-      <div className="select-user-type-content">
+        <div className="select-user-type-logo">
+          <img src={menteegologo} alt="MenteeGo Logo" />
+        </div>
 
-        <h1>Create Account</h1>
+        <div className="select-user-content">
+
+          <div className="select-user-text">
+          <h1>Create Account</h1>
+          <p>Creating an account as mentor or mentee?</p>
+        </div>
 
         <div className="user-type-options">
           <button onClick={handleMentorSelection} className="user-type-button">Mentor</button>
           <button onClick={handleMenteeSelection} className="user-type-button">Mentee</button>
         </div>
 
-      </div>
+        </div>
+
+        
+
       
-    
     </div>
 
   );
